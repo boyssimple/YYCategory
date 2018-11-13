34bc5d98647bb4a857da627d7acb00c86bacab09
罗伟 <luowei@cqyanyu.com>
2018-11-13 17:55:17 +0800

    Initial YYCategory

---
+++ Sources/NSObject+ExtensionAttribute.h
@@ -0,0 +1,17 @@
+//
+//  NSObject+ExtensionAttribute.h
+//  IM
+//
+//  Created by luowei on 2018/11/9.
+//  Copyright © 2018年 luowei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (ExtensionAttribute)
+@property (nonatomic,assign) CGFloat height;
+@end
+
+NS_ASSUME_NONNULL_END
